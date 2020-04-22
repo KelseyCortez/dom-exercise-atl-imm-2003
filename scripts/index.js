ded2a02a430c0d8b91cd54c7686ed16e9ced08fd
KelseyCortez <kelscortez@gmail.com>
2020-04-22 11:30:50 -0400

    am exercises

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,19 @@ const addresses = [
     "https://duckduckgo.com"
 ];
 
+const navLinks = document.createElement('ul');
+const navContainer = document.querySelector(' .js-link-container');
+navContainer.appendChild(navLinks);
+
+function createLink(address) {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.setAttribute('href', address);
+    link.textContent = address;
+    listItem.appendChild(link);
+    navLinks.appendChild(listItem);
+}
+
+addresses.forEach(createLink);
+
+
